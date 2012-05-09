93f93f430c69bf708dccb4e42ad5f0436da9ddd5
casaserena <M8R-ej96pd@mailinator.com>
2012-05-09 09:45:29 -0700

    Added titles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,27 +1,35 @@
 class PagesController < ApplicationController
 
   def index
+    @title = "Welcome to your Sunshine Coast Haven | Casa Serena"
   end
 
   def suite
+    @title = "Sunshine Coast Haven | Suite"
   end
 
   def gallery
+    @title = "Sunshine Coast Haven | Gallery"
   end
 
   def location
+    @title = "Sunshine Coast Haven | Location"
   end
 
   def rates
+    @title = "Sunshine Coast Haven | Rates"
   end
 
   def workshops
+    @title = "Sunshine Coast Haven | Workshops"
   end
 
   def contact
+    @title = "Sunshine Coast Haven | Contact"
   end
 
   def book
+    @title = "Sunshine Coast Haven | Book"
   end
 
 end
